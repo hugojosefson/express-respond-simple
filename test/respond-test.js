1a695fdd522133d68f8593bd1f262792c54212da
Hugo Josefson <hugo@josefson.org>
2019-06-23 12:16:13 +0200

    Upgrade deps.

--- test/respond-test.js
+++ test/respond-test.js
@@ -1,76 +1,76 @@
-var expect = require('chai').expect;
-var respond = require('..');
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+var expect = require('chai').expect
+var respond = require('..')
 
 describe('respond', function () {
+  it('is a function', function () {
+    expect(respond).to.be.a('function')
+  })
 
-    it('is a function', function () {
-        expect(respond).to.be.a.function;
-    });
+  it('does not accept empty input', function () {
+    expect(respond).to.throw('code must')
+  })
 
-    it('does not accept empty input', function () {
-        expect(respond).to.throw('code must');
-    });
+  it('does not accept invalid code', function () {
+    expect(respond.bind({}, 1)).to.throw('invalid status code')
+  })
 
-    it('does not accept invalid code', function () {
-        expect(respond.bind({}, 1)).to.throw('invalid status code');
-    });
+  describe('has property', function () {
+    [
+      'respond200',
+      'respond204',
+      'respond500',
+      'ok',
+      'noContent',
+      'internalServerError'
+    ].forEach(function (prop) {
+      it(prop, function () {
+        expect(respond).has.a.property(prop)
+      })
+    })
+  })
 
-    describe('has property', function () {
-        [
-            'respond200',
-            'respond204',
-            'respond500',
-            'ok',
-            'noContent',
-            'internalServerError'
-        ].forEach(function (prop) {
-            it(prop, function () {
-                expect(respond).has.a.property(prop);
-            });
-        });
-    });
+  describe('is a function', function () {
+    [
+      'respond200',
+      'respond204',
+      'respond500',
+      'ok',
+      'noContent',
+      'internalServerError'
+    ].forEach(function (prop) {
+      it(prop, function () {
+        expect(respond[prop]).is.a('function')
+      })
+    })
+  })
 
-    describe('is a function', function () {
-        [
-            'respond200',
-            'respond204',
-            'respond500',
-            'ok',
-            'noContent',
-            'internalServerError'
-        ].forEach(function (prop) {
-            it(prop, function () {
-                expect(respond[prop]).is.a.function;
-            });
-        });
-    });
+  describe('properties are the same', function () {
+    [
+      ['respond200', 'ok'],
+      ['respond204', 'noContent'],
+      ['respond500', 'internalServerError']
+    ].forEach(function (pair) {
+      it(pair[0] + ' === ' + pair[1], function () {
+        expect(respond[pair[0]]).equal(respond[pair[1]])
+      })
+    })
+  })
 
-    describe('properties are the same', function () {
-        [
-            ['respond200', 'ok'],
-            ['respond204', 'noContent'],
-            ['respond500', 'internalServerError']
-        ].forEach(function (pair) {
-            it(pair[0] + ' === ' + pair[1], function () {
-                expect(respond[pair[0]]).equal(respond[pair[1]]);
-            });
-        });
-    });
-
-    describe('has NOT property', function () {
-        [
-            '0',
-            '1',
-            '200',
-            '299',
-            'respond600',
-            'OK',
-            'Ok'
-        ].forEach(function (prop) {
-            it(prop, function () {
-                expect(respond).has.not.a.property(prop);
-            });
-        });
-    });
-
-});
+  describe('has NOT property', function () {
+    [
+      '0',
+      '1',
+      '200',
+      '299',
+      'respond600',
+      'OK',
+      'Ok'
+    ].forEach(function (prop) {
+      it(prop, function () {
+        expect(respond).has.not.a.property(prop)
+      })
+    })
+  })
+})
--- test/respond200-test.js
+++ test/respond200-test.js
@@ -1,143 +1,143 @@
-var chai = require('chai');
-var spies = require('chai-spies');
-var expect = chai.expect;
-var respond200 = require('..').respond200;
-var respond204 = require('..').respond204;
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+var chai = require('chai')
+var spies = require('chai-spies')
+var expect = chai.expect
+var respond200 = require('..').respond200
+var respond204 = require('..').respond204
 
-chai.use(spies);
+chai.use(spies)
 
 var res = {
-    sendStatus: function sendStatus() {
-        //console.log('sendStatus', arguments);
-        return this;
-    },
-    status: function status() {
-        //console.log('status', arguments);
-        return this;
-    },
-    type: function type() {
-        //console.log('type', arguments);
-        return this;
-    },
-    send: function send() {
-        //console.log('send', arguments);
-        return this;
-    }
-};
-Object.keys(res).forEach(function (key) {
-    chai.spy.on(res, key);
-});
-
-function resetRes() {
-    Object.keys(res).forEach(function (key) {
-        res[key].reset();
-    });
+  sendStatus: function sendStatus () {
+    // console.log('sendStatus', arguments);
+    return this
+  },
+  status: function status () {
+    // console.log('status', arguments);
+    return this
+  },
+  type: function type () {
+    // console.log('type', arguments);
+    return this
+  },
+  send: function send () {
+    // console.log('send', arguments);
+    return this
+  }
 }
 
-beforeEach(resetRes);
-afterEach(resetRes);
+function restoreSpies () {
+  chai.spy.restore()
+  Object.keys(res).forEach(function (key) {
+    chai.spy.on(res, key)
+  })
+}
+
+beforeEach(restoreSpies)
+afterEach(restoreSpies)
 
 describe('respond200', function () {
-    it('is a function', function () {
-        expect(respond200).to.be.a.function;
-    });
-    it('returns a function', function () {
-        expect(respond200()).to.be.a.function;
-    });
-});
+  it('is a function', function () {
+    expect(respond200).to.be.a('function')
+  })
+  it('returns a function', function () {
+    expect(respond200()).to.be.a('function')
+  })
+})
 
 describe('respond204()(req, res)', function () {
-    it('calls res.sendStatus(204)', function () {
-        respond204()(null, res);
-        expect(res.sendStatus).to.have.been.called.once.with(204);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(204)', function () {
+    respond204()(null, res)
+    expect(res.sendStatus).to.have.been.called.once.with(204)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
 
 describe('respond200()(req, res)', function () {
-    it('calls res.sendStatus(204)', function () {
-        respond200()(null, res);
-        expect(res.sendStatus).to.have.been.called.once.with(204);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(204)', function () {
+    respond200()(null, res)
+    expect(res.sendStatus).to.have.been.called.once.with(204)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
 
 describe('respond200("All is good.")(req, res)', function () {
-    it('calls res.status(200).send("All is good.")', function () {
-        respond200('All is good.')(null, res);
-        expect(res.status).to.have.been.called.once.with(200);
-        expect(res.send).to.have.been.called.once.with('All is good.');
-        expect(res.sendStatus).to.not.have.been.called;
-    });
-});
+  it('calls res.status(200).send("All is good.")', function () {
+    respond200('All is good.')(null, res)
+    expect(res.status).to.have.been.called.once.with(200)
+    expect(res.send).to.have.been.called.once.with('All is good.')
+    expect(res.sendStatus).to.not.have.been.called
+  })
+})
 
 describe('respond204("some message")', function () {
-    it('to throw "must be empty"', function () {
-        expect(respond204.bind({}, 'some message')).to.throw('must be empty');
-    });
-});
+  it('to throw "must be empty"', function () {
+    expect(respond204.bind({}, 'some message')).to.throw('must be empty')
+  })
+})
 
 describe('respond200("")(req, res)', function () {
-    it('calls res.sendStatus(204)', function () {
-        respond200('')(null, res);
-        expect(res.sendStatus).to.have.been.called.once.with(204);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(204)', function () {
+    respond200('')(null, res)
+    expect(res.sendStatus).to.have.been.called.once.with(204)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
 
 describe('respond200("", true)(req, res)', function () {
-    it('calls res.status(200).send("")', function () {
-        respond200('', true)(null, res);
-        expect(res.sendStatus).to.not.have.been.called;
-        expect(res.send).to.have.been.called.once.with('');
-        expect(res.status).to.have.been.called.once.with(200);
-    });
-});
+  it('calls res.status(200).send("")', function () {
+    respond200('', true)(null, res)
+    expect(res.sendStatus).to.not.have.been.called
+    expect(res.send).to.have.been.called.once.with('')
+    expect(res.status).to.have.been.called.once.with(200)
+  })
+})
 
 describe('respond204("", true)(req, res)', function () {
-    it('calls res.status(204).send("")', function () {
-        respond204('', true)(null, res);
-        expect(res.sendStatus).to.not.have.been.called;
-        expect(res.send).to.have.been.called.once.with('');
-        expect(res.status).to.have.been.called.once.with(204);
-    });
-});
+  it('calls res.status(204).send("")', function () {
+    respond204('', true)(null, res)
+    expect(res.sendStatus).to.not.have.been.called
+    expect(res.send).to.have.been.called.once.with('')
+    expect(res.status).to.have.been.called.once.with(204)
+  })
+})
 
 describe('respond200(null)(req, res)', function () {
-    it('calls res.sendStatus(204)', function () {
-        respond200(null)(null, res);
-        expect(res.sendStatus).to.have.been.called.once.with(204);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(204)', function () {
+    respond200(null)(null, res)
+    expect(res.sendStatus).to.have.been.called.once.with(204)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
 
 describe('respond200(null, true)(req, res)', function () {
-    it('calls res.sendStatus(200)', function () {
-        respond200(null, true)(null, res);
-        expect(res.sendStatus).to.have.been.called.once.with(200);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(200)', function () {
+    respond200(null, true)(null, res)
+    expect(res.sendStatus).to.have.been.called.once.with(200)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
 
 describe('respond200(undefined)(req, res)', function () {
-    it('calls res.sendStatus(204)', function () {
-        respond200(undefined)(undefined, res);
-        expect(res.sendStatus).to.have.been.called.once.with(204);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(204)', function () {
+    respond200(undefined)(undefined, res)
+    expect(res.sendStatus).to.have.been.called.once.with(204)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
 
 describe('respond200(undefined, true)(req, res)', function () {
-    it('calls res.sendStatus(200)', function () {
-        respond200(undefined, true)(undefined, res);
-        expect(res.sendStatus).to.have.been.called.once.with(200);
-        expect(res.send).to.not.have.been.called;
-        expect(res.status).to.not.have.been.called;
-    });
-});
+  it('calls res.sendStatus(200)', function () {
+    respond200(undefined, true)(undefined, res)
+    expect(res.sendStatus).to.have.been.called.once.with(200)
+    expect(res.send).to.not.have.been.called
+    expect(res.status).to.not.have.been.called
+  })
+})
